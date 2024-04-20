47fba06d4c9ba0b591699c52a36b7d109e37fa10
João Barbosa <contato.jsbarbosa@gmail.com>
2024-04-20 16:50:01 -0300

    Iniciando o projeto - backend

---
+++ backend/src/main/java/com/joaobarbosa/ecommercecatalogv2/EcommerceCatalogV2Application.java
@@ -0,0 +1,13 @@
+package com.joaobarbosa.ecommercecatalogv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCatalogV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCatalogV2Application.class, args);
+	}
+
+}
